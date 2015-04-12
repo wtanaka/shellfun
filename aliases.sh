782a2949cc76a9c799bb7165f400e6aebd045c90
Wesley Tanaka <wtanaka@yahoo.com>
2015-04-12 10:17:00 -1000

    dockviz aliases

--- aliases.sh
+++ aliases.sh
@@ -13,4 +13,6 @@ alias C="cvs commit"
 # https://github.com/CenturyLinkLabs/docker-image-graph
 alias dockgraph="if [ -t 1 ]; then echo outputs a png 1>&2; else sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock centurylink/image-graph; fi"
 # to install, run: go get github.com/justone/dockviz
-alias dockviz='echo -e "GET /images/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz images --tree'
+alias dv-im='echo -e "GET /images/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz images --tree'
+alias dv-container='if [ -t 1 ]; then echo outputs a png 1>&2; else echo -e "GET /containers/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz containers --dot | dot -Tpng; fi'
+alias dv-co='echo -e "GET /containers/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz containers --dot | dot -Tpng -o /tmp/docker-containers.png && eog /tmp/docker-containers.png'
