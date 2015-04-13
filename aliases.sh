354bd553d41092230b5c660952c3e4564eb6a0da
Wesley Tanaka <wtanaka@yahoo.com>
2015-04-12 21:46:29 -1000

    Update docker aliases to remove hyphen
    Add docker alias to run an image with -t -i --rm=true

--- aliases.sh
+++ aliases.sh
@@ -11,8 +11,12 @@ alias gzip="gzip -9"
 alias U="cvs update -dP"
 alias C="cvs commit"
 # https://github.com/CenturyLinkLabs/docker-image-graph
-alias dockgraph="if [ -t 1 ]; then echo outputs a png 1>&2; else sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock centurylink/image-graph; fi"
+alias dkgraph="if [ -t 1 ]; then echo outputs a png 1>&2; else sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock centurylink/image-graph; fi"
 # to install, run: go get github.com/justone/dockviz
-alias dv-im='echo -e "GET /images/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz images --tree'
-alias dv-container='if [ -t 1 ]; then echo outputs a png 1>&2; else echo -e "GET /containers/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz containers --dot | dot -Tpng; fi'
-alias dv-co='echo -e "GET /containers/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz containers --dot | dot -Tpng -o /tmp/docker-containers.png && eog /tmp/docker-containers.png'
+alias dkvi='echo -e "GET /images/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz images --tree'
+alias dkvcontainer='if [ -t 1 ]; then echo outputs a png 1>&2; else echo -e "GET /containers/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz containers --dot | dot -Tpng; fi'
+alias dkvc='echo -e "GET /containers/json?all=1 HTTP/1.0\r\n" | nc -U /var/run/docker.sock | tail -n +5 | dockviz containers --dot | dot -Tpng -o /tmp/docker-containers.png && eog /tmp/docker-containers.png'
+dkrsh()
+{
+  docker run -it --rm=true "$@" sh
+}
